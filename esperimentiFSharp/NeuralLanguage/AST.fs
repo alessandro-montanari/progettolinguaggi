c5323483bb2a0106dd89bd02c97bce68b52b9add
Alessandro Montanari <alemonty88@gmail.com>
2013-01-22 12:06:52 +0100

    Expressions

--- esperimentiFSharp/NeuralLanguage/AST.fs
+++ esperimentiFSharp/NeuralLanguage/AST.fs
@@ -18,13 +18,14 @@ type Expression =
     | Not of Expression
     | Rel of Expression * Relation * Expression
     | Id of string
+    | AttId of string
     | Neg of Expression
     | Num of double
     | Add of Expression * Expression
     | Sub of Expression * Expression
     | Prod of Expression * Expression
     | Frac of Expression * Expression
-    | Pow of Expression * double
+    | Pow of Expression * Expression
     | Sin of Expression
     | Cos of Expression
     | Tan of Expression
@@ -41,9 +42,22 @@ type Expression =
     | Sum of string
     | SumSquared of string
 
+// La sequenza di attributi o di numeri viene espansa in fase di valutazione, quindi
+// si memorizza come tupla di stringhe o di double
+type AttributeSequence = string * string
+type NumberSequence = double * double
+
+type AttributeListElement =
+    | Id of string
+    | Seq of AttributeSequence
+
+type NumberListElement =
+    | Num of double
+    | Seq of NumberSequence
+
 type ParameterValue =
-    | AttList of string list
-    | NumLis of double list
+    | AttList of AttributeListElement list
+    | NumLis of NumberListElement list
     | Val of Expression
 
 type Parameter = string * ParameterValue
@@ -55,7 +69,7 @@ type Aspect = string * string * Parameter list
 type Network =
     {
         Directives : Parameter list;
-        Preprocessing : Filter list * Filter list;
+        Preprocessing : Filter list * Filter list;              // filtri di attributo e di istanza
         NetworkDefinition : string * bool * Parameter list * Aspect list;
         Training : string * Parameter list;
         Validation : (string * Parameter list) option;   // Forse qui conviene fattorizzare in un tipo Validation = string * Parameter list option
