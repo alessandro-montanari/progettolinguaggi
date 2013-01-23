ada72ab0ad560fad84bd8e47bb0cd86ae3e887a5
Alessandro Montanari <alemonty88@gmail.com>
2013-01-23 15:07:03 +0100

    Network (non finito)

--- esperimentiFSharp/NeuralLanguage/Program.fs
+++ esperimentiFSharp/NeuralLanguage/Program.fs
@@ -20,42 +20,29 @@ open NeuralLanguageLex
 // Note:
 [<EntryPoint>]
 let main argv = 
-    let lexbuf = Lexing.LexBuffer<_>.FromString "p0 : -0.7 ; \n\
-                                                 p1 : ATT4+4.0; \n\
-                                                 p2 : myatt*-4.9; \n\
-                                                 p3 : 5.0 + 6.0 +7.0; \n\
-                                                 p4 : 5.0 - 6.0 - 7.0; \n\
-                                                 p5 : 5.0 > 6.0 > 7.0; \n\
-                                                 p6 : (3.0+5.0)*(9.0 -2.0); \n\
-                                                 p7 : [ciao, ciao, ciao]; \n\
-                                                 p8 : [ciao, ATT0, ATT6 .. ATT10]; \n\
-                                                 p9 : [4.0, 5.9, 5.6 .. 90.99]; \n\
-                                                 p10 : true && false + sin(4.0); \n\
-                                                 p11 : 4.5 + -5; \n\
-                                                 p12 : [ATT3, \"mio _ * - ^ att\", ATT9..ATT20]; \n\
-                                                 p13 : 5^3^2; \n\
-                                                 p14: !4.5 && !7; \n\
-                                                 p15 : -(3+4*5); \n\
-                                                 p16 : !(5); \n\
-                                                 p17 : mean(ciao); \n\
-                                                 p18 : mean(\"ciao ciao\"); \n\
-                                                 p19 : mean(ATT5); \n\
-                                                 p20 : [INST1, INST3, INST0..INST6, INST7]; \n\
-                                                 p21 : ![ATT4, ATT6] \n\
-                                                 PREPROCESSING \n\
-                                                 { \n\
-                                                    TRAINING_SET : \"C:\ciao\ciao\ciao.arff\" \n\
+    let lexbuf = Lexing.LexBuffer<_>.FromString "PREPROCESSING \n\
+                                                 { TRAINING_SET : \"C:\ciao\ciao\ciao.arff\" \n\
                                                     ATTRIBUTE \n\
+                                                    { } \n\
+                                                    INSTANCE \n\
+                                                    { } \n\
+                                                  } \n\
+                                                  \n\
+                                                  MultiLayerNetwork  \n\
+                                                  { \n\
+                                                    p17 : mean(ciao) \n\
+                                                    INPUT_LAYER in \n\
+	                                                { \n\
+                                                        p17 : mean(ciao), \n\
+                                                        p11 : 4.5 + -5 \n\
+                                                    }, \n\
+                                                    \n\
+                                                    HIDDEN_LAYER primo \n\
                                                     { \n\
-                                                        filter1(par:6, par2:x+y, par3:ATT3+ATT5); \n\
-                                                        filter1(par:6, par2:x+y, par3:ATT3+ATT5) \n\
+                                                        p17 : mean(ciao), \n\
+                                                        p11 : 4.5 + -5 \n\
                                                     } \n\
                                                     \n\
-                                                    INSTANCE \n\
-                                                    { \n\
-                                                        filter1(par:6, par2:x+y, par3:ATT3+ATT5); \n\
-                                                        filter1() \n\
-                                                    }\n\
                                                   }" 
     try
         printfn "%A" (NeuralLanguageParser.start NeuralLanguageLex.tokenize lexbuf)
@@ -65,3 +52,58 @@ let main argv =
     System.Console.ReadLine() |> ignore
     0
  
+
+// let bigTest = "p0 : -0.7 ; \n\
+//                p1 : ATT4+4.0; \n\
+//                p2 : myatt*-4.9; \n\
+//                p3 : 5.0 + 6.0 +7.0; \n\
+//                p4 : 5.0 - 6.0 - 7.0; \n\
+//                p5 : 5.0 > 6.0 > 7.0; \n\
+//                p6 : (3.0+5.0)*(9.0 -2.0); \n\
+//                p7 : [ciao, ciao, ciao]; \n\
+//                p8 : [ciao, ATT0, ATT6 .. ATT10]; \n\
+//                p9 : [4.0, 5.9, 5.6 .. 90.99]; \n\
+//                p10 : true && false + sin(4.0); \n\
+//                p11 : 4.5 + -5; \n\
+//                p12 : [ATT3, \"mio _ * - ^ att\", ATT9..ATT20]; \n\
+//                p13 : 5^3^2; \n\
+//                p14: !4.5 && !7; \n\
+//                p15 : -(3+4*5); \n\
+//                p16 : !(5); \n\
+//                p17 : mean(ciao); \n\
+//                p18 : mean(\"ciao ciao\"); \n\
+//                p19 : mean(ATT5); \n\
+//                p20 : [INST1, INST3, INST0..INST6, INST7]; \n\
+//                p21 : ![ATT4, ATT6] \n\
+//                PREPROCESSING \n\
+//                { \n\
+//                TRAINING_SET : \"C:\ciao\ciao\ciao.arff\" \n\
+//                ATTRIBUTE \n\
+//                { \n\
+//                    filter1(par:6, par2:x+y, par3:ATT3+ATT5); \n\
+//                    filter1(par:6, par2:x+y, par3:ATT3+ATT5) \n\
+//                } \n\
+//                \n\
+//                INSTANCE \n\
+//                { \n\
+//                    filter1(par:6, par2:x+y, par3:ATT3+ATT5); \n\
+//                    filter1() \n\
+//                }\n\
+//                } \n\
+//                \n\
+//                MultiLayerNetwork WITH_BIAS \n\
+//                { \n\
+//                p17 : mean(ciao) \n\
+//                INPUT_LAYER in \n\
+//	            { \n\
+//                    p17 : mean(ciao); \n\
+//                    p11 : 4.5 + -5 \n\
+//                } \n\
+//                \n\
+//                HIDDEN_LAYER primo \n\
+//                { \n\
+//                    p17 : mean(ciao); \n\
+//                    p11 : 4.5 + -5 \n\
+//                } \n\
+//                \n\
+//                }"
