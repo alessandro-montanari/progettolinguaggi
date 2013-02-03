698b19f7955b3f6bb95d398b6a33721ba7dadc9b
Alessandro Montanari <alemonty88@gmail.com>
2013-02-03 10:02:48 +0100

    Changes

--- esperimentiFSharp/NeuralNetwork/Graph.fs
+++ esperimentiFSharp/NeuralNetwork/Graph.fs
@@ -11,6 +11,7 @@ let createGraphFromNetwork (nn:MultiLayerNetwork) =
 
     nn.OutputLayer
     |> Seq.iter (fun el ->  let targetNode = graph.AddEdge(el.Value.Id.ToString(), el.Key).TargetNode
+                            
                             targetNode.Attr.Shape <- Microsoft.Glee.Drawing.Shape.Box
                             targetNode.Attr.Fillcolor <- Microsoft.Glee.Drawing.Color.Yellow
                             el.Value.InputMap.Keys
@@ -30,3 +31,58 @@ let createGraphFromNetwork (nn:MultiLayerNetwork) =
     viewer.Graph <- graph;
     viewer.Dock <- System.Windows.Forms.DockStyle.Fill;
     viewer
+
+
+// Selezione nodo o ramo
+//object selectedObjectAttr;
+//    object selectedObject;
+//    void gViewer_SelectionChanged(object sender, EventArgs e)
+//    {
+//
+//      if (selectedObject != null)
+//      {
+//        if (selectedObject is Edge)
+//          (selectedObject as Edge).Attr = selectedObjectAttr as EdgeAttr;
+//        else if (selectedObject is Node)
+//          (selectedObject as Node).Attr = selectedObjectAttr as NodeAttr;
+//
+//        selectedObject = null;
+//      }
+//
+//      if (gViewer.SelectedObject == null)
+//      {
+//        label1.Text = "No object under the mouse";
+//        this.gViewer.SetToolTip(toolTip1, "");
+//
+//      }
+//      else
+//      {
+//        selectedObject = gViewer.SelectedObject;
+//      
+//        if (selectedObject is Edge)
+//        {
+//          selectedObjectAttr = (gViewer.SelectedObject as Edge).Attr.Clone();
+//          (gViewer.SelectedObject as Edge).Attr.Color = Microsoft.Glee.Drawing.Color.Magenta;
+//          (gViewer.SelectedObject as Edge).Attr.Fontcolor = Microsoft.Glee.Drawing.Color.Magenta;
+//          Edge edge=gViewer.SelectedObject as Edge;
+//       
+//         
+//
+//
+//        //here you can use e.Attr.Id or e.UserData to get back to you data
+//          this.gViewer.SetToolTip(this.toolTip1, String.Format("edge from {0} {1}", edge.Source, edge.Target));
+//     
+//        }
+//        else if (selectedObject is Node)
+//        {
+//
+//          selectedObjectAttr = (gViewer.SelectedObject as Node).Attr.Clone();
+//          (selectedObject as Node).Attr.Color = Microsoft.Glee.Drawing.Color.Magenta;
+//          (selectedObject as Node).Attr.Fontcolor = Microsoft.Glee.Drawing.Color.Magenta;
+//          //here you can use e.Attr.Id to get back to your data
+//          this.gViewer.SetToolTip(toolTip1,String.Format("node {0}", (selectedObject as Node).Attr.Label));
+//        }
+//        label1.Text = selectedObject.ToString();
+//      }
+//      gViewer.Invalidate();
+//    }
